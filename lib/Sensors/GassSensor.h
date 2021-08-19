ff871742b050406237655674cad1023f3120fa9c
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2021-08-19 21:48:51 +0430

    Update gassSensor service
    Get gassLevel

--- lib/Sensors/GassSensor.h
+++ lib/Sensors/GassSensor.h
@@ -5,10 +5,15 @@ class GassSensor
 public:
     bool isEmergencySituation(int analogPin, float rate = 2)
     {
-        float gassLevel = analogRead(analogPin) / 100.0;
+        float gassLevel = getLevel(analogPin);
         Serial.println("Gass level: " + String(gassLevel));
         if (gassLevel > rate)
             return true;
         return false;
+    }
+    
+    float getLevel(int analogPin)
+    {
+        return analogRead(analogPin) / 100.0;
     }
 };
